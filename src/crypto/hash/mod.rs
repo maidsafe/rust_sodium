7bf59050b71e479c12f85661074847859ef7f086
Chu Ka-cheong <kcchu@users.noreply.github.com>
2017-10-22 12:20:54 +0800

    Add HashState for multi-parts (Init-Update-Final) hash computation.
    Fix #119.

--- src/crypto/hash/mod.rs
+++ src/crypto/hash/mod.rs
@@ -36,6 +36,11 @@
 //!
 //! let data_to_hash = b"some data";
 //! let digest = hash::hash(data_to_hash);
+//!
+//! let mut hash_state = hash::HashState::new();
+//! hash_state.update(b"some ");
+//! hash_state.update(b"data!");
+//! let digest = hash_state.finish();
 //! ```
 
 pub use self::sha512::*;
