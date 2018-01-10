598848dbe1f22ffcd878d74059a5060d160c6420
dnaq <dnaq@users.noreply.github.com>
2018-01-10 13:23:34 +0100

    Make `crypto::sealedbox::SEALBYTES` public.
    Closes #184

--- src/crypto/sealedbox/curve25519blake2bxsalsa20poly1305.rs
+++ src/crypto/sealedbox/curve25519blake2bxsalsa20poly1305.rs
@@ -8,7 +8,7 @@ use libc::c_ulonglong;
 
 /// Number of additional bytes in a ciphertext compared to the corresponding
 /// plaintext.
-const SEALBYTES: usize = ffi::crypto_box_SEALBYTES as usize;
+pub const SEALBYTES: usize = ffi::crypto_box_SEALBYTES as usize;
 
 /// The `seal()` function encrypts a message `m` for a recipient whose public key
 /// is `pk`. It returns the ciphertext whose length is `SEALBYTES + m.len()`.
